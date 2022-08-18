9e0111cecd2ca98ad71ee10672bfba9544974dd5
Anne Lyle <annelyle@ebi.ac.uk>
2022-08-18 13:10:48 +0000

    Hide nodes for which we have no data

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -27,7 +27,8 @@ sub update_conf {
   $SiteDefs::ENSEMBL_RELEASE_DATE = '22 September 2021';
   $SiteDefs::RAPID_RELEASE_VERSION = 25;
   $SiteDefs::NO_REGULATION            = 1;
-  $SiteDefs::NO_VARIATION             = 1;
+  $SiteDefs::NO_VARIATION             = 0;
+  $SiteDefs::VCF_VARIANTS_ONLY        = 1;
   $SiteDefs::NO_COMPARA               = 0;
   $SiteDefs::SINGLE_SPECIES_COMPARA   = 1;
   $SiteDefs::ENSEMBL_MART_ENABLED     = 0;
