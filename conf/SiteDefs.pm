fa2b588da618a9a1614317ac65ae844754fd2ded
Imran Salam <imran@ebi.ac.uk>
2023-05-10 10:51:47 +0000

    RR-46 updated ini files and configurations

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -24,8 +24,8 @@ package EnsEMBL::RapidRelease::SiteDefs;
 sub update_conf {
   $SiteDefs::ENSEMBL_SUBTYPE          = 'Rapid Release';
   $SiteDefs::FIRST_RELEASE_VERSION    = 100; ## Don't update this!
-  $SiteDefs::ENSEMBL_RELEASE_DATE = '11 April 2023';
-  $SiteDefs::RAPID_RELEASE_VERSION = 45;
+  $SiteDefs::ENSEMBL_RELEASE_DATE = '11 May 2023';
+  $SiteDefs::RAPID_RELEASE_VERSION = 46;
   $SiteDefs::HAS_ANNOTATION           = 1;
   $SiteDefs::NO_REGULATION            = 1;
   $SiteDefs::NO_VARIATION             = 0;
