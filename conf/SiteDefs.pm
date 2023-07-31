7f39ad12139e5e349d0fc79b8ba9228c86892e15
Jyothish <joyjoy.nt@gmail.com>
2023-07-31 17:48:01 +0000

    RR-49 updated ini files and configurations

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -24,8 +24,8 @@ package EnsEMBL::RapidRelease::SiteDefs;
 sub update_conf {
   $SiteDefs::ENSEMBL_SUBTYPE          = 'Rapid Release';
   $SiteDefs::FIRST_RELEASE_VERSION    = 100; ## Don't update this!
-  $SiteDefs::ENSEMBL_RELEASE_DATE = '29 June 2023';
-  $SiteDefs::RAPID_RELEASE_VERSION = 48;
+  $SiteDefs::ENSEMBL_RELEASE_DATE = '02 August 2023';
+  $SiteDefs::RAPID_RELEASE_VERSION = 49;
   $SiteDefs::HAS_ANNOTATION           = 1;
   $SiteDefs::NO_REGULATION            = 1;
   $SiteDefs::NO_VARIATION             = 0;
